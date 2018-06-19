5b0ccc1ded4b4e07f5d2c95d38f93902a69eb163
VOVANCHO <w-karpov@mail.ru>
2018-06-19 12:52:11 +0300

    added UsersController

---
+++ commands/UsersController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\commands;
+
+use yii\base\Module;
+use yii\console\Controller;
+use yii\console\ExitCode;
+use app\domain\services\UserService;
+use yii\console\widgets\Table;
+
+class UsersController extends Controller
+{
+    private $service;
+
+    public function __construct($id, Module $module, array $config = [], UserService $service)
+    {
+        $this->service = $service;
+        parent::__construct($id, $module, $config);
+    }
+
+    public function actionFriendsRecommendation(int $count = 1)
+    {
+        echo Table::widget([
+            'headers' => ['user_id', 'recommend_id', 'rate'],
+            'rows' => $this->service->getFriendsRecommendation($count),
+        ]);
+
+        return ExitCode::OK;
+    }
+}
